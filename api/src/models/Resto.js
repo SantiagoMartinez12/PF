01119445e316d2f1ec1c261ba5f09737742ca926
Sergio Illescas <illescas.s.m@gmail.com>
2021-11-22 18:36:14 -0300

    rutas resto a revisar

--- api/src/models/Resto.js
+++ api/src/models/Resto.js
@@ -1,49 +1,52 @@
-const { DataTypes} = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('resto', {
-    id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      primaryKey:true,
-    },
-
-    qr:{
+  sequelize.define(
+    "resto",
+    {
+      id: {
         type: DataTypes.STRING,
-    },
+        allowNull: false,
+        primaryKey: true,
+      },
 
-    image: {
+      qr: {
+        type: DataTypes.STRING,
+      },
+
+      image: {
         type: DataTypes.BLOB,
-    },
+      },
 
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-
-    usuario: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-
-    contraseña: {
+      name: {
         type: DataTypes.STRING,
         allowNull: false,
-    },
+      },
 
-    mail:{
+      usuario: {
         type: DataTypes.STRING,
         allowNull: false,
-    },
-    
-    mesa:{
+      },
+
+      contraseña: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      mail: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      mesa: {
         type: DataTypes.INTEGER,
         allowNull: false,
+      },
+    },
+    {
+      freezeTableName: true,
     }
-
-  },{
-    freezeTableName: true
-    });
+  );
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,24 +1,21 @@
-const { Router } = require('express');
-const restoRoutes = require('../routes/resto')
-const mesaRoutes = require('../routes/mesa')
-const categoriasRoutes = require('../routes/categorias')
-const productoRoutes = require('../routes/producto')
-const detalleRoutes = require('../routes/detalle')
+const { Router } = require("express");
+const restoRoutes = require("../routes/resto");
+const mesaRoutes = require("../routes/mesa");
+const categoriasRoutes = require("../routes/categorias");
+const productoRoutes = require("../routes/producto");
+const detalleRoutes = require("../routes/detalle");
 
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 // router.use('/mesa', mesaRoutes)
-// router.use('/resto', restoRoutes)
+router.use("/resto", restoRoutes);
 // router.use('/categorias', categoriasRoutes)
 // router.use('/producto', productoRoutes)
-router.use('/detalle', detalleRoutes)
-
-
+router.use("/detalle", detalleRoutes);
 
 module.exports = router;
