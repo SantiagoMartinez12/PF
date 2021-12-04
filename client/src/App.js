7c7e99b0b7855198c73e46a265918c0ad65d157b
Sergio Illescas <illescas.s.m@gmail.com>
2021-12-04 08:46:25 -0300

    para pullreq

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,13 @@ import HomeClient from "./components/Cliente/homeCliente/homeCliente.jsx";
 import Setting from "./components/Resto/setting/setting";
 import HomeResto from "./components/Resto/home/home";
 import Detalle from "./components/Resto/home/detalle";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import ErrorQr from "./components/Cliente/landingPageCliente/errorQr";
-import LandingPageResto from "./components/Resto/landingPageResto/landigPageResto"
+import LandingPageResto from "./components/Resto/landingPageResto/landigPageResto";
 import { useDispatch } from "react-redux";
 import modificarUsuario from "./store/actions";
 import Usuario from "./components/Resto/setting/usuario";
-
 
 function App() {
   return (
@@ -21,15 +20,16 @@ function App() {
       <Routes>
         <Route exact path="/:idResto/:idMesa" element={<LandingPageClient />} />
 
-        <Route exact path="/:idResto/:idMesa/home/:name" element={<HomeClient />}/>
+        <Route
+          exact
+          path="/:idResto/:idMesa/home/:name"
+          element={<HomeClient />}
+        />
         <Route exact path="/" element={<LandingPageResto />} />
-        <Route exact path="/home/resto/:restoId" element={<HomeResto/>}/>
+        <Route exact path="/home/resto/:restoId" element={<HomeResto />} />
         <Route path="/home/resto/setting/:restoId" element={<Setting />} />
         <Route exact path="/errorQr" element={<ErrorQr />} />
-        <Route exact path="/detalle/:idCliente" element={<Detalle/>}/>
-   
-
-
+        <Route exact path="/detalle/:idCliente" element={<Detalle />} />
       </Routes>
     </div>
   );
