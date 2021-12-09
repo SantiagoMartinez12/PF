c1ac3f5030b568bea107248a096dd07e162b2d32
pato0358 <patricio.randle@gmail.com>
2021-12-09 17:45:09 -0300

    filtros productos

--- client/src/components/Resto/product/Product.jsx
+++ client/src/components/Resto/product/Product.jsx
@@ -2,53 +2,126 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ProductsPerCategory from "./ProductsPerCategory";
 import FormProduct from "./FormProduct";
-import './Product.css';
+import "./Product.css";
 import ProductDetail from "./ProductDetail";
 import { getProductos } from "../../../store/actions";
 import { useParams } from "react-router";
+import Buscador from "./Buscador";
+import { filtroProductos} from '../../../store/actions';
 
 const Product = () => {
-    const [form, setForm] = useState(false)
-    const [showDetail, setShowDetail] = useState(false);
-    const [infoDetail, setInfoDetail] = useState({});
-    const dispatch = useDispatch();
-    const data = useSelector((state)=>state.rawData)
-    const {restoId} = useParams()
+  const [form, setForm] = useState(false);
+  const [showDetail, setShowDetail] = useState(false);
+  const [showFiltrados, setShowFiltrados] = useState(false);
+  const [showCategorias, setShowCategorias] = useState(false);
+  const [infoDetail, setInfoDetail] = useState({});
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.rawData);
+  const { restoId } = useParams();
+  const filtrados = useSelector((state) => state.productosFiltrados);
 
-    useEffect(() => {
-        dispatch(getProductos(restoId));
+  useEffect(() => {
+    dispatch(getProductos(restoId));
+  }, []);
 
-    }, [])
+  const handleAddProduct = (e) => {
+    e.preventDefault();
+    setForm(true);
+  };
+  const handleReset = (e) => {
+    e.preventDefault();
+    setShowFiltrados(false);
+    setShowCategorias(false);
+  };
+  const handleFiltroCategorias = (e) => {
+    dispatch(filtroProductos(e.target.value),
+    setShowCategorias(true))
+  };
 
-    const handleAddProduct = (e) => {
-        e.preventDefault();
-        setForm(true);
-    }
-
-    return (
+  return (
+    <>
+      <div>
+        <Buscador setShowFiltrados={setShowFiltrados} />
+      </div>
+      {showFiltrados ? (
         <>
-            {showDetail ? <ProductDetail info={infoDetail} /> :
-                form ? <FormProduct /> :
-                    <div>
-                        <div>
-                            <button type='button' onClick={(e) => handleAddProduct(e)} className="btn btn-primary me-md-2">Agregar Producto</button>
-                        </div>
-                        <div>
-
-                            {data[0] ? data.map((categories) => {
-                                return <div>
-                                    <h4>{categories.name}</h4>
-                                    <ProductsPerCategory productos={categories.productos} setShowDetail={setShowDetail} setInfoDetail={setInfoDetail} />
-                                </div>
-                            })
-                        :<h2>No hay productos cargados</h2>
-                        }
-
-                        </div>
-                    </div>
-            }
+          <button type="button" className="btn btn-primary me-md-2" onClick={(e) => handleReset(e)}>
+            Volver
+          </button>
+          <ProductsPerCategory
+            productos={filtrados}
+            setShowDetail={setShowDetail}
+            setInfoDetail={setInfoDetail}
+          />
         </>
-    )
-}
+      ) : showCategorias ? (
+        <>
+        <button type="button" className="btn btn-primary me-md-2" onClick={(e) => handleReset(e)}>
+            Volver
+          </button>
+        <ProductsPerCategory
+            productos={filtrados}
+            setShowDetail={setShowDetail}
+            setInfoDetail={setInfoDetail}
+          />
+          </>
+      ) :     
+      
+      showDetail ? (
+        <ProductDetail info={infoDetail} />
+      ) : form ? (
+        <FormProduct />
+      ) : (
+        <div>
+          <div>
+            <button
+              type="button"
+              onClick={(e) => handleAddProduct(e)}
+              className="btn btn-primary me-md-2"
+            >
+              Agregar Producto
+            </button>
+          </div>
+
+          <div>
+            <h4>Buscar por categoría:</h4>
+            <select onChange={(e) => handleFiltroCategorias(e)}>
+              <option key="All" value="All">
+                {" "}
+                Todas
+              </option>
+              {data[0]
+                ? data.map((categories) => {
+                    return (
+                      <option key={categories.name} value={categories.name}>
+                        {categories.name}
+                      </option>
+                    );
+                  })
+                : null}
+            </select>
+
+            {data[0] ? (
+              data.map((categories) => {
+                return (
+                  <div>
+                    <h4>{categories.name}</h4>
+                    <ProductsPerCategory
+                      productos={categories.productos}
+                      setShowDetail={setShowDetail}
+                      setInfoDetail={setInfoDetail}
+                    />
+                  </div>
+                );
+              })
+            ) : (
+              <h2>No hay productos cargados</h2>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default Product;
