1f6fd9d0c77dbe7ac001b329858c9f6bc6dab6d7
k0th <israel@relativobranding.com>
2021-12-11 10:36:36 -0600

    Nueva version 8

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import ErrorQr from "./components/Cliente/landingPageCliente/errorQr";
 import LandingPageResto from "./components/Resto/landingPageResto/landigPageResto";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import MesaOcupada from "./components/Cliente/landingPageCliente/mesaOcupada";
 import Gracias from "./components/Cliente/homeCliente/gracias";
 import SuperAdmin from "./components/Admin/Admin";
