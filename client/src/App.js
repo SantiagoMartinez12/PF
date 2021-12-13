d6a7baab6e94a946ae819af8b37c7c2f8bc93d12
MayBourdal <maybourdal6@gmail.com>
2021-12-13 18:53:02 -0300

    Ta listo papa

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App() {
       <Routes>
         <Route exact path="/:idResto/:idMesa" element={<LandingPageClient />} />
         <Route exact path="/:idResto/:idMesa/home/:name/:idCliente" element={<HomeClient />}/>
-        <Route exact path="/admin" element={<SuperAdmin/>}/>
+        <Route exact path="/admin/:idAdmin" element={<SuperAdmin/>}/>
         <Route exact path="/" element={<LandingPageResto />} />
         <Route exact path="/home/resto/:restoId" element={<HomeResto />} />
         <Route path="/home/resto/setting/:restoId" element={<Setting />} />
